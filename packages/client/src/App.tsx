453ecffc32583bed76c7ab976550f7b86dd05484
DmitriyYanenko <paletobay@mail.ru>
2024-05-19 14:55:36 +0300

    feature/cfg-1

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -12,7 +12,9 @@ function App() {
 
     fetchServerData()
   }, [])
-  return <div className="App">Вот тут будет жить ваше приложение :)</div>
+  return (
+    <div className="App">Вот тут будет жить ваше приложение :) CodersForce</div>
+  )
 }
 
 export default App
