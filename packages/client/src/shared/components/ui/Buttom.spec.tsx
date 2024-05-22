35f133d123ff3547d1793071f6d4231c3ac6de8a
DmitriyYanenko <paletobay@mail.ru>
2024-05-22 16:58:47 +0300

    feature/cfg-34

---
+++ packages/client/src/shared/components/ui/Buttom.spec.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+
+import '@testing-library/jest-dom'
+import BaseButton from './Button'
+
+describe('<BaseButton />', () => {
+  const handleClick = jest.fn()
+
+  it('Snapshot is correct', () => {
+    const { asFragment } = render(<BaseButton onClick={handleClick} />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('Renders correctly with default props', () => {
+    render(<BaseButton />)
+    const button = screen.getByRole('button')
+
+    expect(button).toMatchSnapshot()
+  })
+})
