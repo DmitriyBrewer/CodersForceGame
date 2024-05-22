4d23de79f82088699835dab2eee1872ff0c246c3
DmitriyYanenko <paletobay@mail.ru>
2024-05-22 16:23:41 +0300

    feature/cfg-34

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -4,12 +4,13 @@ import dotenv from 'dotenv'
 import path from 'path'
 dotenv.config()
 
+// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: Number(process.env.CLIENT_PORT) || 3000,
   },
   define: {
-    SERVER_PORT: process.env.SERVER_PORT,
+    __SERVER_PORT__: process.env.SERVER_PORT,
   },
   plugins: [react()],
   resolve: {
