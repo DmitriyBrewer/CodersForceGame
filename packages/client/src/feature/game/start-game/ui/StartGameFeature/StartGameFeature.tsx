40ad042b5ef719a862e414c95168719786b34e05
DmitriyYanenko <paletobay@mail.ru>
2024-05-31 12:22:00 +0300

    update

--- packages/client/src/feature/game/start-game/ui/StartGameFeature/StartGameFeature.tsx
+++ packages/client/src/feature/game/start-game/ui/StartGameFeature/StartGameFeature.tsx
@@ -1,27 +1,17 @@
 import { FC } from 'react'
 
-import BaseLoader from '@/shared/components/ui/BaseLoader'
-
-import { useRenderClient } from '@/shared/hooks/useRenderClient'
-
 import { useStartGame } from '../../hooks/useStartGame'
 import StartGame from '../StartGame/StartGame'
 import StopGame from '../StopGame'
 
 const StartGameFeature: FC = () => {
-  const isRenderClient = useRenderClient()
-
   const { progress, isGameStarted, isButtonDisabled, handleStartGame, handleStopGame } = useStartGame()
 
-  if (isRenderClient && !isGameStarted) {
+  if (!isGameStarted) {
     return <StartGame progress={progress} isButtonDisabled={isButtonDisabled} handleStartGame={handleStartGame} />
   }
 
-  if (isRenderClient && isGameStarted) {
-    return <StopGame handleStopGame={handleStopGame} />
-  }
-
-  return !isRenderClient && <BaseLoader />
+  return <StopGame handleStopGame={handleStopGame} />
 }
 
 export default StartGameFeature
--- packages/client/src/shared/components/ui/BaseLoader/BaseLoader.tsx
+++ packages/client/src/shared/components/ui/BaseLoader/BaseLoader.tsx
@@ -1,5 +1,5 @@
 import { CircularProgress } from '@mui/material'
-
+// TODO:feature/cfg-37 удалить позже, если не будет используется
 const BaseLoader = CircularProgress
 
 export default BaseLoader
