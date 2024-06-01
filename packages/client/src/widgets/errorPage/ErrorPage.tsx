59d33a25bc0aa246485183a434be9244a7cfed47
Misha <fxmxa@proton.me>
2024-06-01 12:54:18 +0400

    Вынесены стили

--- packages/client/src/widgets/errorPage/ErrorPage.tsx
+++ packages/client/src/widgets/errorPage/ErrorPage.tsx
@@ -2,24 +2,38 @@ import BaseTypography from '@/shared/components/ui/BaseTypography'
 import BaseBox from '@/shared/components/ui/BaseBox'
 import BaseLink from '@/shared/components/ui/BaseLink'
 
+import styles from './ErrorPage.module.scss'
+
 type Props = {
   errorNumber: string
   errorText: string
 }
+const cssProps = {
+  title: {
+    fontWeight: 'var(--fw700)',
+    fontSize: 'var(--fs160)'
+  },
+  subTitle: {
+    fontWeight: 'var(--fw600)'
+  },
+  link: {
+    mt: 'var(--m20)'
+  }
+}
 
 const ErrorPageWidget = ({ errorNumber, errorText }: Props) => {
   return (
-    <BaseBox display="flex" alignItems="center" justifyContent="center" height="100vh" flexDirection="column">
-      <BaseTypography variant="h4" fontWeight="600" color="primary.dark">
+    <BaseBox className={styles.root}>
+      <BaseTypography variant="h4" color="primary.dark" {...cssProps.subTitle}>
         Ууупс!
       </BaseTypography>
-      <BaseTypography variant="h1" fontWeight="700" fontSize="160px" color="error.dark">
+      <BaseTypography variant="h1" color="error.dark" {...cssProps.title}>
         {errorNumber}
       </BaseTypography>
       <BaseTypography variant="h6" color="primary.dark">
         {errorText}
       </BaseTypography>
-      <BaseLink to="/" mt="100px">
+      <BaseLink to="/" {...cssProps.link}>
         На главную
       </BaseLink>
     </BaseBox>
