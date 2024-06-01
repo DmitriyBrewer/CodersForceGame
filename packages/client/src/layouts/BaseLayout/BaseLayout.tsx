4c551c944530815101e675454ae453d8361425da
Misha <fxmxa@proton.me>
2024-06-01 10:35:18 +0400

    удалил BaseContainer из layout

--- packages/client/src/layouts/BaseLayout/BaseLayout.tsx
+++ packages/client/src/layouts/BaseLayout/BaseLayout.tsx
@@ -1,8 +1,6 @@
 import { FC } from 'react'
 import { Outlet } from 'react-router-dom'
 
-import BaseContainer from '@/shared/components/ui/BaseContainer'
-
 const BaseLayout: FC = () => {
   return (
     // TODO feature/cfg-28 сделать тест ErrorBoundary
@@ -12,9 +10,7 @@ const BaseLayout: FC = () => {
         <h4>тут будет хедер</h4>
       </header>
       <main>
-        <BaseContainer>
-          <Outlet />
-        </BaseContainer>
+        <Outlet />
       </main>
       <footer>
         <h4>тут будет футер</h4>
