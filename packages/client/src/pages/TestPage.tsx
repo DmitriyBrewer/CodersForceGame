44eabb6a51ce6be50f0f82173859dd198b0645e0
DmitriyYanenko <paletobay@mail.ru>
2024-06-03 14:07:29 +0300

    feature/cfg-61

--- packages/client/src/pages/TestPage.tsx
+++ packages/client/src/pages/TestPage.tsx
@@ -4,12 +4,16 @@ import { styleMui } from '@/shared/styleMui'
 import BaseBox from '@/shared/components/ui/BaseBox'
 import BaseTypography from '@/shared/components/ui/BaseTypography'
 import BasePaper from '@/shared/components/ui/BasePaper'
+
+import styles from './TestPage.module.scss'
 // TODO  feature/cfg-61 удалить позже
 
 const TestPage: FC = () => {
   return (
     <BaseBox sx={styleMui.testBgComponent}>
-      <BaseTypography sx={styleMui.errorTypograpy}>Текст ошибки</BaseTypography>
+      <BaseTypography sx={styleMui.errorTypograpy} className={styles.errorTypography}>
+        Текст ошибки
+      </BaseTypography>
       <BasePaper sx={styleMui.paperInfo}>информационный Paper с кастомным фоном</BasePaper>
     </BaseBox>
   )
