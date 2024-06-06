e9c7327a66bd1cc299795c6a1840f7db6cf50044
DmitriyYanenko <paletobay@mail.ru>
2024-06-06 15:50:20 +0300

    leaderboard to widget

--- packages/client/src/app/router/AppRouter.tsx
+++ packages/client/src/app/router/AppRouter.tsx
@@ -4,7 +4,6 @@ import { paths } from '@/shared/config/routing'
 
 import RegisterPage from '@/pages/session/register'
 import Profile from '@/pages/Profile'
-import Leaderboard from '@/pages/leaderboard'
 import Forum from '@/pages/Forum'
 import ForumTopic from '@/pages/ForumTopic'
 import Login from '@/pages/Login'
@@ -13,6 +12,8 @@ import Page404 from '@/pages/404'
 import Error500Page from '@/pages/error/Error500'
 
 import StartGamePage from '@/pages/game/game-engine'
+
+import LeaderBoardPage from '@/pages/leaderboard/ui'
 
 import PrivateRoute from '@/app/router/PrivateRoute'
 import PublicRoute from '@/app/router/PublicRoute'
@@ -40,7 +41,7 @@ const router = createBrowserRouter([
       },
       {
         path: paths.leaderboard,
-        element: <PrivateRoute component={Leaderboard} />
+        element: <PrivateRoute component={LeaderBoardPage} />
       },
       {
         path: paths.forum,
--- packages/client/src/pages/Leaderboard/ui/index.tsx
+++ packages/client/src/pages/Leaderboard/ui/index.tsx
@@ -1,29 +1,9 @@
-import BasicTable from '@/shared/components/ui/BaseTable'
-import BaseTypography from '@/shared/components/ui/BaseTypography'
-import BaseContainer from '@/shared/components/ui/BaseContainer'
+import { FC } from 'react'
 
-// TODO: feature/cfg-24 получить данные с сервера
-const LeaderBoard = () => {
-  const data = [
-    { rank: 1, name: 'Frozen yoghurt', points: 303, email: 'some@mail.ru' },
-    { rank: 2, name: 'Cupcake', points: 248, email: 'some2@mail.ru' },
-    { rank: 3, name: 'Gingerbread', points: 101, email: 'some3@mail.ru' }
-  ]
+import LeaderBoardWidget from '@/widgets/leaderboard'
 
-  const headers = [
-    { text: 'Место', value: 'rank' },
-    { text: 'Имя', value: 'name' },
-    { text: 'Очки', value: 'points' }
-  ]
-
-  return (
-    <BaseContainer>
-      <BaseTypography component="h1" variant="h4" my="var(--s28)">
-        Лучшие результаты игры
-      </BaseTypography>
-      <BasicTable headers={headers} data={data} />
-    </BaseContainer>
-  )
+const LeaderBoardPage: FC = () => {
+  return <LeaderBoardWidget />
 }
 
-export default LeaderBoard
+export default LeaderBoardPage
