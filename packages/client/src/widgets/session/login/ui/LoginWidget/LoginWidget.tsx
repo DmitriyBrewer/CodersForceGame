9085032efa8b6f6c57deb9c922151b840d73e1c0
DmitriyYanenko <paletobay@mail.ru>
2024-06-15 15:06:42 +0300

    full height

--- packages/client/src/widgets/session/login/ui/LoginWidget/LoginWidget.tsx
+++ packages/client/src/widgets/session/login/ui/LoginWidget/LoginWidget.tsx
@@ -6,16 +6,22 @@ import BaseContainer from '@/shared/components/ui/BaseContainer'
 
 import { styleMui } from '@/shared/styleMui'
 
+import BaseBox from '@/shared/components/ui/BaseBox'
+
 import LoginFeature from '@/feature/session/login/ui'
 
 import backgroundImage from '@/assets/images/login-background.png'
 
+import styles from './LoginWidget.module.scss'
+
 const LoginWidget: FC = () => {
   return (
     <BackgroundWrapper backgroundUrl={backgroundImage} filter="blur(2px)">
-      <BaseContainer maxWidth="sm" sx={styleMui.secsionContainer}>
-        <LoginFeature />
-      </BaseContainer>
+      <BaseBox className={styles.box}>
+        <BaseContainer maxWidth="sm" sx={styleMui.secsionContainer}>
+          <LoginFeature />
+        </BaseContainer>
+      </BaseBox>
     </BackgroundWrapper>
   )
 }
--- packages/client/src/widgets/session/register/ui/RegisterWidget/RegisterWidget.tsx
+++ packages/client/src/widgets/session/register/ui/RegisterWidget/RegisterWidget.tsx
@@ -6,16 +6,22 @@ import BackgroundWrapper from '@/shared/components/core/BackgroundWrapper'
 
 import { styleMui } from '@/shared/styleMui'
 
+import BaseBox from '@/shared/components/ui/BaseBox'
+
 import RegisterFeature from '@/feature/session/register'
 
 import backgroundImage from '@/assets/images/login-background.png'
 
+import styles from './RegisterWidget.module.scss'
+
 const RegisterWidget: FC = () => {
   return (
     <BackgroundWrapper backgroundUrl={backgroundImage} filter="blur(2px)">
-      <BaseContainer maxWidth="sm" sx={styleMui.secsionContainer}>
-        <RegisterFeature />
-      </BaseContainer>
+      <BaseBox className={styles.box}>
+        <BaseContainer maxWidth="sm" sx={styleMui.secsionContainer}>
+          <RegisterFeature />
+        </BaseContainer>
+      </BaseBox>
     </BackgroundWrapper>
   )
 }
