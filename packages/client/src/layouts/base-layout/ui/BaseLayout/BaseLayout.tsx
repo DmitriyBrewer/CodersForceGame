524c8a6c95c26d9a856e7376e7eb864d625299e7
DmitriyYanenko <paletobay@mail.ru>
2024-06-17 13:21:18 +0300

    global height layout

--- packages/client/src/layouts/base-layout/ui/BaseLayout/BaseLayout.tsx
+++ packages/client/src/layouts/base-layout/ui/BaseLayout/BaseLayout.tsx
@@ -4,12 +4,14 @@ import { Outlet } from 'react-router-dom'
 import NavbarWidget from '@/widgets/base-layout/navbar'
 import FooterWidget from '@/widgets/base-layout/footer/FooterWidget'
 
+import styles from './BaseLayout.module.scss'
+
 const BaseLayout: FC = () => {
   return (
     // TODO feature/cfg-28 сделать тест ErrorBoundary
     <>
       <NavbarWidget />
-      <main>
+      <main className={styles.root}>
         <Outlet />
       </main>
       <FooterWidget />
