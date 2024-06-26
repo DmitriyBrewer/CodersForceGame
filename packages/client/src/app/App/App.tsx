b3863f2b05500b3b4050985c4583ebccebb1b8b2
DmitriyYanenko <paletobay@mail.ru>
2024-06-26 11:35:30 +0300

    cfg-88

--- packages/client/src/app/App/App.tsx
+++ packages/client/src/app/App/App.tsx
@@ -1,26 +1,69 @@
-import { CssBaseline } from '@mui/material'
-import { Provider } from 'react-redux'
+// import { CssBaseline } from '@mui/material'
+// import { Provider } from 'react-redux'
 
-import store from '@/shared/store'
-import ErrorBoundary from '@/shared/context/error-boundary'
-import ThemeModeProvider from '@/shared/context/theme-provider'
+import { Suspense, useEffect, useState } from 'react'
 
-import GlobalErrorWrapper from '@/shared/context/global-error-wrapper'
+// import store from '@/shared/store'
+// import ErrorBoundary from '@/shared/context/error-boundary'
+// import ThemeModeProvider from '@/shared/context/theme-provider'
 
-import AppRouter from '../router'
+// import GlobalErrorWrapper from '@/shared/context/global-error-wrapper'
+
+// import AppRouter from '../router'
 
 const App = () => {
+  // const [mounted, setMounted] = useState(false)
+  // useEffect(() => {
+  //   setMounted(true)
+  // }, [])
+  // useEffect(() => {
+  //   const fetchServerData = async () => {
+  //     const url = `http://localhost:${__SERVER_PORT__}/api`
+  //     const response = await fetch(url)
+  //     const data = await response.json()
+  //     console.log(data)
+  //   }
+
+  //   fetchServerData()
+  // }, [])
+
+  // const [mounted, setMounted] = useState(false)
+  const [test, setTest] = useState('olo')
+
+  useEffect(() => {
+    setTest('llo')
+  }, [])
+
+  // useEffect(() => {
+  //   setMounted(true)
+  // }, [])
+
+  useEffect(() => {
+    const fetchServerData = async () => {
+      const url = `http://localhost:${__SERVER_PORT__}/api`
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+    }
+
+    fetchServerData()
+  }, [])
+
   return (
-    <Provider store={store}>
-      <ErrorBoundary>
-        <ThemeModeProvider>
-          <CssBaseline />
-          <GlobalErrorWrapper>
-            <AppRouter />
-          </GlobalErrorWrapper>
-        </ThemeModeProvider>
-      </ErrorBoundary>
-    </Provider>
+    <Suspense>
+      <div className="App">Hello world :) {test}</div>
+    </Suspense>
+
+    // <Provider store={store}>
+    //   <ErrorBoundary>
+    //     <ThemeModeProvider>
+    //       <CssBaseline />
+    //       <GlobalErrorWrapper>
+    //         <AppRouter />
+    //       </GlobalErrorWrapper>
+    //     </ThemeModeProvider>
+    //   </ErrorBoundary>
+    // </Provider>
   )
 }
 
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,15 +1,41 @@
 import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
+import { hydrateRoot } from 'react-dom/client'
+
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import App from './app/App'
 
-import '@/app/styles/global.module.scss'
-import registerServiceWorker from '@/app/service-worker/registerServiceWorker'
+// import App from './app/App'
 
-registerServiceWorker()
+// import '@/app/styles/global.module.scss'
+// import registerServiceWorker from '@/app/service-worker/registerServiceWorker'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// registerServiceWorker()
+
+hydrateRoot(
+  document.getElementById('root') as HTMLElement,
   <StrictMode>
     <App />
   </StrictMode>
 )
+// <StrictMode>
+//   <App />
+// </StrictMode>
+// <BrowserRouter>
+// {
+//   /* <App /> */
+// }
+// {
+//   /* <Routes>
+//       <Route element={<h1>Hello</h1>} path="/" key={1} />
+//     </Routes> */
+// }
+// {
+//   /* <Routes>
+//       <Route element={<App />} path="/" key={1} />
+//     </Routes> */
+// }
+// {
+//   /* <h1>Hello</h1>
+//   </BrowserRouter> */
+// }
