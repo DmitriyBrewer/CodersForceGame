aac010961bd50282dea3e7fef2e03c7af87f6d34
Anastasiia <hlyzova.an17@gmail.com>
2024-07-02 16:44:11 +0500

    add ui test

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -11,11 +11,13 @@ export default {
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/src/__mocks__/fileMock.js'
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/src/__mocks__/fileMock.js',
+    '\\.(css|scss)$': 'jest-css-modules'
   },
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  roots: ['<rootDir>/src']
+  roots: ['<rootDir>/src'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTest.ts']
 }
