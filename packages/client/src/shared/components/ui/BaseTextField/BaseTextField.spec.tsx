1468dc698fc317458e7aac93ce2e58683f98b3ec
Anastasiia <hlyzova.an17@gmail.com>
2024-07-04 14:22:30 +0500

    check testAvatar

--- packages/client/src/shared/components/ui/BaseTextField/BaseTextField.spec.tsx
+++ packages/client/src/shared/components/ui/BaseTextField/BaseTextField.spec.tsx
@@ -1,16 +1,24 @@
 import { render, screen } from '@testing-library/react'
+import { act } from 'react'
 
 import '@testing-library/jest-dom'
 import BaseTextField from './BaseTextField'
 
 describe('<BaseTextField />', () => {
   it('Snapshot is correct', () => {
-    const { asFragment } = render(<BaseTextField />)
+    let asFragment: () => DocumentFragment = () => document.createDocumentFragment()
+
+    act(() => {
+      const result = render(<BaseTextField />)
+      asFragment = result.asFragment
+    })
     expect(asFragment()).toMatchSnapshot()
   })
 
   it('Renders correctly with default props', () => {
-    render(<BaseTextField />)
+    act(() => {
+      render(<BaseTextField />)
+    })
     const textbox = screen.getByRole('textbox')
 
     expect(textbox).toBeInTheDocument()
