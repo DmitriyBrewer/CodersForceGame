4f13683e29a57518154d653daf74cc0ed862a3b7
DmitriyYanenko <paletobay@mail.ru>
2024-07-18 17:10:17 +0300

    feature/cfg-95

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,27 +1,40 @@
 import dotenv from 'dotenv'
 import cors from 'cors'
 import { ViteDevServer, createServer as createViteServer } from 'vite'
-import express from 'express'
-import serialize from 'serialize-javascript'
 
-import type { RenderResult } from 'client/src/shared/types'
+import express from 'express'
 
 import * as fs from 'fs'
 import * as path from 'path'
 
 dotenv.config()
 
-const isDev = () => process.env.NODE_ENV === 'development'
-
+// TODO:feature/cfg-95 позже вернуть process.env.NODE_ENV
+// const isDev = () => process.env.NODE_ENV === 'development'
+const isDev = () => false
 async function startServer() {
   const app = express()
   app.use(cors())
   const port = Number(process.env.SERVER_PORT) || 3001
 
   let vite: ViteDevServer | undefined
-  const distPath = path.dirname(require.resolve('client/dist/index.html'))
-  const srcPath = path.dirname(require.resolve('client'))
-  const ssrClientPath = require.resolve('client/ssr-dist/client.cjs')
+  let distPath = ''
+  let srcPath = ''
+  let ssrClientPath = ''
+
+  console.log(distPath)
+  console.log(srcPath)
+  console.log(ssrClientPath)
+
+  if (isDev()) {
+    distPath = path.dirname(require.resolve('client/dist/index.html'))
+    srcPath = path.dirname(require.resolve('client'))
+    ssrClientPath = require.resolve('client/ssr-dist/client.cjs')
+  } else {
+    distPath = path.resolve('/app/packages/client/dist')
+    srcPath = path.resolve('/app/packages/client')
+    ssrClientPath = path.resolve('/app/client/ssr-dist/client.cjs')
+  }
 
   if (isDev()) {
     vite = await createViteServer({
@@ -46,35 +59,38 @@ async function startServer() {
     const url = req.originalUrl
 
     try {
-      let template: string | undefined
+      let template: string
 
       if (!isDev()) {
         template = fs.readFileSync(path.resolve(distPath, 'index.html'), 'utf-8')
       } else {
         template = fs.readFileSync(path.resolve(srcPath, 'index.html'), 'utf-8')
-        template = await vite?.transformIndexHtml(url, template)
+
+        // TODO: feature/cfg-88 тут оставим из-за особенностей vite, позже удалить сообщение
+        //  eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        template = await vite!.transformIndexHtml(url, template)
       }
 
-      let render: (requestUrl: string) => Promise<RenderResult>
+      let render: (requestUrl: string) => Promise<string>
 
       if (!isDev()) {
         render = (await import(ssrClientPath)).render
       } else {
-        render = (await vite?.ssrLoadModule(path.resolve(srcPath, 'src/entry-server.tsx')))?.render
+        // TODO: feature/cfg-88 тут оставим из-за особенностей vite, позже удалить сообщение
+        //  eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        render = (await vite!.ssrLoadModule(path.resolve(srcPath, 'src/entry-server.tsx'))).render
       }
 
-      const { appHtml, preloadedState } = await render(url)
-      const html = template?.replace('<!--ssr-outlet-->', appHtml).replace(
-        `<!--ssr-initial-state-->`,
-        `<script>window.APP_INITIAL_STATE = ${serialize(preloadedState, {
-          isJSON: true
-        })}</script>`
-      )
+      const appHtml = await render(url)
+
+      const html = template.replace('<!--ssr-outlet-->', appHtml)
 
       res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
     } catch (e) {
       if (isDev()) {
-        vite?.ssrFixStacktrace(e as Error)
+        // TODO: feature/cfg-88 тут оставим из-за особенностей vite, позже удалить сообщение
+        //  eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        vite!.ssrFixStacktrace(e as Error)
       }
       next(e)
     }
